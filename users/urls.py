a07f6954ccfae6cd7c1fb81ccf23f3e0c9a61a20
lizaalomidzeee <lomidzelizi997@gmail.com>
2025-03-27 19:16:20 +0400

    change

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from users.views import UserViewSet, RegisterViewSet, ProfileViewSet
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('register', RegisterViewSet, basename='register')
-router.register('users', ProfileViewSet, basename='users')
+router.register('users', ProfileViewSet, basename='users_profile')
 
 urlpatterns = [
     path('', include(router.urls))
